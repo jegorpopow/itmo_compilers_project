8d5e4aa10e03f1477c345ce05bb719ec5a4bd76a
Pavel Grigorenko <GrigorenkoPV@niuitmo.ru>
2026-02-26 23:33:11 +0300

    tests: check that lexer has all test cases listed

--- xtask/src/tests.rs
+++ xtask/src/tests.rs
@@ -31,3 +31,24 @@ fn all_files_are_used() {
     check_has_all_tests(&lexer_tests, &srcs)?;
     check_has_all_tests(&srcs, &lexer_tests)?;
 }
+
+#[test]
+#[throws]
+fn lexer_has_all_test_cases() {
+    let lexer_tests_file = workspace_root()?.join("compiler/src/lexer/tests.rs");
+    let actual = fs::read_to_string(&lexer_tests_file).with_context(|| {
+        format!("Failed to read the file with lexer tests ({lexer_tests_file:?})")
+    })?;
+    let expected = lexer_tests()?;
+    #[expect(
+        clippy::iter_over_hash_type,
+        reason = "Do we really care about order of errors?"
+    )]
+    for name in &expected.names {
+        ensure!(
+            actual.contains(&*format!(" => {name:?},")),
+            "Test case {name:?} is missing in {lexer_tests_file:?} (expected because {:?} exists)",
+            expected.name_to_path(name)
+        )
+    }
+}
