cf3937de6114be5371e16549d5d4a695e778849c
Pavel Grigorenko <GrigorenkoPV@niuitmo.ru>
2026-02-27 02:13:44 +0300

    xtask: clap

---
+++ xtask/src/cli.rs
@@ -0,0 +1,33 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, about)]
+#[command(
+    bin_name = "cargo x",
+    arg_required_else_help = true,
+    help_expected = true
+)]
+#[derive(Debug)]
+pub(crate) enum Task {
+    /// Update test cases listed in lexer src based on tests/ dir content
+    UpdateLexerTests,
+}
+
+impl Task {
+    #[must_use]
+    #[expect(clippy::same_name_method, reason = "hiding clap under the rug")]
+    pub(crate) fn parse() -> Self {
+        <Self as Parser>::parse()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn verify_cli() {
+        use clap::CommandFactory;
+        Task::command().debug_assert()
+    }
+}
