a2c5d6a330bd6e2a6922cb399e23b4fd6d0e89e3
Pavel Grigorenko <GrigorenkoPV@niuitmo.ru>
2026-03-29 16:18:45 +0300

    vm: nix

---
+++ vm/package.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  stdenv,
+  cmake,
+  gtest,
+  gflags,
+  fmt_11,
+  CMAKE_BUILD_TYPE ? "Release",
+}:
+let
+  compiler = stdenv.cc.meta.name;
+  pname = "vm";
+  version = "${CMAKE_BUILD_TYPE}-${compiler}";
+  mainProgram = "${pname}-${CMAKE_BUILD_TYPE}";
+in
+stdenv.mkDerivation {
+  inherit pname version;
+
+  src = ./.;
+
+  nativeBuildInputs = [ cmake ];
+  cmakeFlags = [ "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}" ];
+  buildInputs = [
+    gtest
+    gflags
+    fmt_11
+  ];
+
+  doCheck = true;
+
+  postInstall = ''
+    mv $out/bin/{${pname},${mainProgram}}
+  '';
+
+  meta = {
+    inherit mainProgram;
+    branch = CMAKE_BUILD_TYPE;
+    platforms = lib.platforms.linux;
+  };
+}
